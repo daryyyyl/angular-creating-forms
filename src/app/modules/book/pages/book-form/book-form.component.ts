57ff19286b6fd3f0916c81a2814568e6674275de
daryyyyl <daryl.victa15@gmail.com>
2023-01-18 14:38:05 +0800

    two-way binding

--- src/app/modules/book/pages/book-form/book-form.component.ts
+++ src/app/modules/book/pages/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-book-form',
@@ -9,21 +10,32 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class BookFormComponent implements OnInit {
 
   bookForm: FormGroup
+  authorsArray: FormArray
 
   constructor(private fb: FormBuilder) {
     this.bookForm = this.fb.group({
       name: [''],
+      authors: this.fb.array([
+        new FormControl(''), 
+        new FormControl(''), 
+        new FormControl('')
+      ]),
       isbn: ['']
     })
+    this.authorsArray = this.bookForm.get('authors') as FormArray
   }
 
   ngOnInit(): void {
     this.bookForm.valueChanges.subscribe(data => {
+      console.clear()
       console.log(data)
     })
   }
 
   onSubmit = () => {
-    console.log(this.bookForm.value)
+    //console.log(this.antiHeroForm.value)
+    // const myAntiHero:AntiHero = this.antiHeroForm.value as AntiHero
+    const myBookForm:Book = this.bookForm.getRawValue() as Book
+    console.log('myBookForm', myBookForm)
   }
 }
